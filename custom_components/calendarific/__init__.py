55fff37df2c8a9245b07998422ce0649f730dec7
kernelpanic85 <80057019+kernelpanic85@users.noreply.github.com>
2025-10-08 10:16:16 -0400

    Update __init__.py to fix HA deprecations 2

--- custom_components/calendarific/__init__.py
+++ custom_components/calendarific/__init__.py
@@ -55,13 +55,13 @@ def setup(hass, config):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
     return True
 
 async def async_unload_entry(hass, entry):
     """Unload a config entry."""
-    return await hass.config_entries.async_forward_entry_unloads(entry, "sensor")
+    return await hass.config_entries.async_forward_entry_unloads(entry, ["sensor"])
 
 class CalendarificApiReader:
 
